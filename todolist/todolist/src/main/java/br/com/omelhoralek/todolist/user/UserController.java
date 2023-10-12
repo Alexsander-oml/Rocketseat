05797e190223e98754a7fdc41a62b7d4c56644e0
AlexsanderCS <alexsander2016.acs@gmail.com>
2023-10-12 14:12:27 -0300

    Criando Users

--- todolist/todolist/src/main/java/br/com/omelhoralek/todolist/user/UserController.java
+++ todolist/todolist/src/main/java/br/com/omelhoralek/todolist/user/UserController.java
@@ -1,5 +1,7 @@
 package br.com.omelhoralek.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.SecurityProperties.User;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +17,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UserController {
 
+  @Autowired
+  private IUserRepository userRepository;
+
   /*
    * String (texto)
    * Integer (int) numeros inteiros
@@ -29,8 +34,14 @@ public class UserController {
    * body
    */
   @PostMapping("/")
-  public void create(@RequestBody UserModel UserModel) {
-    System.out.println(UserModel.getUsername());
+  // public void create(@RequestBody UserModel UserModel) {
+    public UserModel create(@RequestBody UserModel UserModel) {
+    
+    // System.out.println(UserModel.getUsername());
+    
+    this.userRepository.save(UserModel);
+    var userCreated = this.userRepository.save(UserModel);
+    return userCreated;
   }
 
 }
